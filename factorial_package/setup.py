d49936b802b440f9deb0349d84866cf36a8e8c4b
Harshvardhan Jadhav <jadhavharshvardhan6301@gmail.com>
2023-07-24 14:18:14 +0530

    Update setup.py

--- factorial_package/setup.py
+++ factorial_package/setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mypackage',
-    version='1.0',
+    name='Testing_packages',
+    version='0.1',
     packages=find_packages(),
+    install_requires=[],
+    author='Harsh Jadhav',
+    author_email='jadhavharshvardhan6301@gmail.com',
+    description='A simple Python package for testing',
+    url='https://github.com/Harshj6301/Testing_packages',
 )
