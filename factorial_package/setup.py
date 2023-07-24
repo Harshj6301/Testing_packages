3c034b5f7c95a3cbfd56d910a40fc1ac24b45625
Harshvardhan Jadhav <jadhavharshvardhan6301@gmail.com>
2023-07-24 14:26:31 +0530

    Update setup.py

--- factorial_package/setup.py
+++ factorial_package/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Testing_packages',
+    name='mypackages',
     version='0.1',
     packages=find_packages(),
     install_requires=[],
